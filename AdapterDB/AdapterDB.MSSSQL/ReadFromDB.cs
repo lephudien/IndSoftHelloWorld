3d4f6a30f031eff4d18a070bbf7e933e4f82a67e
Anh Tuan Nguyen <tuan.anh.nguyen@plus4u.net>
2020-09-30 15:34:55 +0200

    Zalozeni

---
+++ AdapterDB/AdapterDB.MSSSQL/ReadFromDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDB.MSSSQL
+{
+  public class ReadFromDB
+  {
+    public static string GetMyVersion()
+    {
+      return "AdapterDB.MSSSQLVer=1.1.0.0" + Environment.NewLine + "Using package System.Data.SqlClient 4.8.2";
+    }
+  }
+}
---
+++ AdapterDB/AdapterDB.Oracle/ReadFromDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDB.Oracle
+{
+  public class ReadFromDB
+  {
+    public static string GetMyVersion()
+    {
+      return "AdapterDB.OracleVer=1.2.0.0" + Environment.NewLine + "Using package Oracle.ManagedDataAccess 19.9";
+    }
+  }
+}
