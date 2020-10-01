52ebc860cd2cbfffaf3ca1882e23754c007207cd
Anh Tuan Nguyen <tuan.anh.nguyen@plus4u.net>
2020-10-01 09:59:15 +0200

    Test Common Link

--- AdapterDB/AdapterDB.Oracle/ReadFromDB.cs
+++ AdapterDB/AdapterDB.Oracle/ReadFromDB.cs
@@ -12,13 +12,17 @@ namespace AdapterDB.Oracle
   {
     public static string GetMyVersion()
     {
+      int a = Common.Constants.CONSTANTY.C_NEJAKA_VERZE;
+
       var test = new OracleNotificationSource();
       //Debug.Assert(test == null);
 
       System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
       System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
 
-      return "AdapterDB.OracleVer=" + fvi.FileVersion + Environment.NewLine + "Using package Oracle.ManagedDataAccess 19.9";
+      return $"AdapterDB.OracleVer={fvi.FileVersion} {Environment.NewLine} Using package Oracle.ManagedDataAccess 19.9 {a}";
+
+
     }
   }
 }
---
+++ Common/common.cs
@@ -0,0 +1,9 @@
+
+namespace Common.Constants
+{
+	public class CONSTANTY
+{
+	public const int C_NEJAKA_VERZE = 5; 
+} 
+
+}
