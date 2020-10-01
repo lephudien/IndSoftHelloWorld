96aba6d82a79ac3472ed6b0fb2ba30d58b462c97
Anh Tuan Nguyen <tuan.anh.nguyen@plus4u.net>
2020-10-01 14:20:39 +0200

    TestPackageOldStyle

---
+++ TestPackageOldStyle/TestPackageOldStyle/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPackageOldStyle
+{
+  public class Test
+  {
+    public static string GetMyVersion()
+    {
+      var test = new Google.Apis.Logging.ConsoleLogger(Google.Apis.Logging.LogLevel.All); ;
+      //Debug.Assert(test == null);
+
+      System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+      System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+
+      return $"TestPackageOldStyleVer={fvi.FileVersion} {Environment.NewLine} Using package Google.Apis.Logging";
+
+
+    }
+  }
+}
